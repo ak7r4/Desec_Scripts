b258b402a4a5388672163317890e82cf5988f3ee
ak7r4 <67960625+ak7r4@users.noreply.github.com>
2023-11-13 23:42:30 -0300

    Create meta.sh
    Script to see metadata of files from google, with the extension that you selected

---
+++ Bash_Scripts/meta.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Requirements: exiftool and lynx
+#Run this script in a empty folder
+#DANGER: This Script will download all files with the extension selected
+target="$1"
+file="$2"
+
+if [ "$1" == "" ]
+then
+	echo "---------------------------------------------------"
+	echo "Support any extension (pdf, doc, xlsx, ppt, etc...)"
+	echo "---------------------------------------------------"
+	echo "Usage: ./meta.sh target.com doc"
+	echo "---------------------------------------------------"
+elif [ "$2" == "" ]
+then
+	echo "---------------------------------------------------"
+	echo "Support any extension (pdf, doc, xlsx, ppt, etc...)"
+	echo "---------------------------------------------------"
+	echo "Usage: ./meta.sh alvo.com doc"
+	echo "---------------------------------------------------"
+else
+	lynx --dump "https://google.com/search?&q=site:$target+ext:$file" | grep ".pdf" | cut -d "=" -f2 | egrep -v "site|google" | sed 's/...$//' > urls.temp
+	for url in $(cat urls.temp); do wget -q $url; done 
+	rm urls.temp
+	exiftool *.$file
+fi
